4bbc9023d345d79242b12e2366f4b1b6d81e7469
Dung Nguyen <clemenation@gmail.com>
2013-04-15 09:46:36 +0700

    Restructure project folder; Add new EVViewFlipper

---
+++ EasyVocab/Class/View/EVViewFlipper.h
@@ -0,0 +1,13 @@
+//
+//  EVViewFlipper.h
+//  EasyVocab
+//
+//  Created by Dung Nguyen on 4/15/13.
+//  Copyright (c) 2013 ICT54. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EVViewFlipper : NSObject
+
+@end
---
+++ EasyVocab/Class/View/EVViewFlipper.m
@@ -0,0 +1,13 @@
+//
+//  EVViewFlipper.m
+//  EasyVocab
+//
+//  Created by Dung Nguyen on 4/15/13.
+//  Copyright (c) 2013 ICT54. All rights reserved.
+//
+
+#import "EVViewFlipper.h"
+
+@implementation EVViewFlipper
+
+@end
